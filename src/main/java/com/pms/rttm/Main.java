b8ccd34b73f27c7940afd7a9e30d8dc126dd2b82
sureshvasantha <sureshrbe22@gmail.com>
2025-12-22 18:19:20 +0530

    initial commit

---
+++ src/main/java/com/pms/rttm/Main.java
@@ -0,0 +1,16 @@
+package com.pms.rttm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
